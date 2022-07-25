80da85a2ea029a9390bf6e5b1ab5cbb004e17bdf
Mayur Gade <mayurgade13@hotmail.com>
2022-07-25 21:11:57 +0530

    main

---
+++ app/src/main/java/com/test/memory_leaks_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.test.memory_leaks_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
